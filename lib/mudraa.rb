d404633423278bceb98a6bcd1b0479f574afab96
Abhimanyu-1855 <abhimanyu@cogoport.com>
2022-10-17 18:41:09 +0530

    fix

--- lib/mudraa.rb
+++ lib/mudraa.rb
@@ -1,10 +1,6 @@
-# frozen_string_literal: true
-
-require_relative "mudraa/version"
-
-# Dir.glob(File.join( '**', '*.rb'), base: 'lib').each do |file|
-#   require file
-# end
+Dir.glob(File.join( '**', '*.rb'), base: 'lib').each do |file|
+  require file
+end
 
 module Mudraa
   class Error < StandardError; end
