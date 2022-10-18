39a0a94da6c8b8a3d7eedae487e610f026691195
Abhimanyu-1855 <108576288+Abhimanyu-1855@users.noreply.github.com>
2022-10-18 12:05:42 +0530

    Update mudraa.rb

--- lib/mudraa.rb
+++ lib/mudraa.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 
-Dir.glob(File.join( '**', '*.rb'), base: 'lib').each do |file|
-  require file
-end
+Dir.glob(File.join( '**', '*.rb'), base: 'lib').each {|file| require file }
 
 module Mudraa
   class Error < StandardError; end
